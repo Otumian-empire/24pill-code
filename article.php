eae3ece50a0adc81ed766a1e8cca2225f3de53ea
otumian <popecan1000@gmail.com>
2019-08-29 14:11:01 +0000

    modified article - I slowing down

--- article.php
+++ article.php
@@ -38,16 +38,51 @@
 			<!-- content -->
 			<div><?= decode_data(str_replace("rn", "<br>", $article_data['post_content']));?></div>
 			<br><br>
+
 			<div class="text-area">
-				<textarea class="comment-box p-1 md-textarea form-control rounded-0" placeholder="Place your comments here" type="textarea" id="comment-box" name="comment-box"></textarea>
-				<br>
-				<div class="input-group">
-					<span class="counter">500</span>
-					<button class="btn btn-success border-left-0 border" id="add-comment-btn">ADD</button>
-				</div>
+				<form action="" method="get">
+					<textarea class="comment-box p-1 md-textarea form-control rounded-0" placeholder="Place your comments here" type="textarea" id="comment-box" name="comment-box"></textarea>
+					<br>
+					<div class="input-group">
+						<span class="counter">500</span>
+						<button class="btn btn-success border-left-0 border" id="add-comment-btn" name="add-comment-btn">ADD</button>
+					</div>
+				</form>
+				
 			</div>
 
 		</div>
+
+		<!-- TODO: Move this code into the controller, it will be more secure there -->
+		<!-- php code for adding comment to a post, insert into db -->
+		<?php
+			/* if (isset($_GET['add-comment-btn']) && isset($_GET['comment-box'])) {
+
+				if (!empty($_GET['comment-box'])) {
+					// comment_id, post_id, comment_text, comment_date, user_email
+					// default  ,$_GET['qid'], $user_comment, default, $user_email
+
+					$post_id = $_GET['qid'];
+					$comment_text = check_data($_GET['comment-box']);
+					$user_email = get_user_email();
+
+					$insert_comment_query = "INSERT INTO `comments`(`post_id`, `comment_text`, `user_email`) VALUES ({$post_id}, {$comment_text}, {$user_email})";
+
+					echo "{$post_id}, {$comment_text}, {$user_email}";
+
+				} else {
+					// redirect_to("article.php?qid=" . $_GET['qid'] . "&error_msg=you can reply with an empty comment");
+					echo "you can reply with an empty comment";
+					// exit;
+				}
+			} else {
+				// redirect_to("article.php?qid=" . $_GET['qid'] . "&error_msg=you have to sign in or up ...");
+				echo "you have to sign in or up ...";
+				// exit;
+			} */
+		?>
+		<!-- read comments from db on the page -->
+		
 
 		<!-- comment -->
 		<div class="comments">
