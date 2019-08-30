347ba8930eed6431de0fd88451a0dc60ab6c0280
otumian <popecan1000@gmail.com>
2019-08-30 07:38:14 +0000

    fixed typo in index.php and all_articles.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 
 ?>
 
+
 <div class="index-body container">
     <?php
         if ($article_data):
@@ -39,7 +40,7 @@
     ?>      <div class="card mx-auto mt-5 card-register text-center">
                 <div class="card-header text-uppercase">NO Articles</div>
                 <div class="card-body text-lowercase">
-                    <p>Sorry Note articles here yet!! <a href="write_article.php">Add</a> an article..</p>
+                    <p>sorry no articles here yet!! <a href="write_article.php">Add</a> an article..</p>
                 </div>
                 
             </div>
