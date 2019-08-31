3208c88bb1413469c6628307bbfbde874b4368f0
otumian <popecan1000@gmail.com>
2019-08-31 09:22:08 +0000

    added a simple footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,53 @@
     </div>
+
+    <footer>
+        <div class="footer" id="footer">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <h4> Brand Name </h4>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-2 col-xs-3">
+                        <h3> Contact </h3>
+                        <ul>
+                            <li><a class="email"href="#"> insert email here </a></li>
+                            <br/>
+                            <li> <p> address line one </p> </li>
+                            <li> <p> address line two </p> </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-2 col-xs-3">
+                        <ul>
+                            <li> <h5> <a href="#" style="margin-top: 5em"> ABOUT US</a> <h5></li>
+                            <li> <h5><a href="#"> CURRENT SERIES </a> <h5></li>
+                            <li> <h5><a href="#"> THE HOUSE </a> <h5></li>
+                            <li> <h5><a href="#"> LOOKING BACK </a> <h5></li>
+                        </ul>
+                    </div>
+
+                    <!--/.row--> 
+                </div>
+
+                <!--/.container--> 
+            </div>
+
+            <!--/.footer-->
+
+            <div class="footer-bottom">
+                <div class="container">
+                <p class="pull-left copyright"> Copyright © Footer 2014. All right reserved. </p>
+
+            </div>
+        </div>
+        <!--/.footer-bottom--> 
+
+    </footer>
+
     <script src="statics/js/jquery-3.4.1.js"></script>
     <script src="statics/js/bootstrap.js"></script>
     <!-- <script src="statics/js/comments.js"></script> -->
