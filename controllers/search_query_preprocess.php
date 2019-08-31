f4250d273e51eeaea9207032d7cfc84d15d9c0ec
otumian <popecan1000@gmail.com>
2019-08-31 17:24:54 +0000

    try to add a form into the navbar to send request for a search

---
+++ controllers/search_query_preprocess.php
@@ -0,0 +1,5 @@
+<?php
+    include_once "controller_preprocessor.php"
+?>
+
+
--- templates/navigation_bar.php
+++ templates/navigation_bar.php
@@ -11,36 +11,37 @@
 <div class="navigation-bar container-fluid col-sm-12 navbar navbar-expand-lg box fixed-top">
 
     <!-- logo -->
-    <div class="col-sm-1">
-        <a class="navbar-brand" href="./">
+    <div class="col-sm-1 col-md-1 col-lg-1 mx-auto">
+        <a class="navbar-brand col-sm-12 col-md-12 col-lg-12" href="./">
             <img src="<?="statics/img/24pill-code-blue.png";?>" alt="logo-brand" width="40" height="30">
         </a>
     </div>
 	
     <!-- nav links -->
-    <div class="col-sm-3">
+    <div class="col-sm-2 col-md-2 col-lg-2 mx-auto">
         <a class="btn btn-sm btn-outline-primary" href="<?="./";?>">Recent</a>
         <a class="btn btn-sm btn-outline-primary" href="<?="all_articles.php";?>">Articles</a>
     </div>
     
     <!-- search bar -->
-     <div class="input-group col-sm-5">
-        <input class="form-control py-2 border-right-0 border" type="search" placeholder="search by title, tag and or email" id="example-search-input">
-        <span class="input-group-append">
-            <button class="btn btn-outline-secondary border-left-0 border" type="button">
-                <i class="fa fa-search"></i>
-            </button>
-        </span>
+     <div class="input-group col-sm-6 col-md-6 col-lg-6">
+        <form action="controllers/search_query_preprocess.php" method="get" class="form-inline">
+            <input class="form-control-sm border border-right-0 col-lg-6" type="search" placeholder="search by title, tag and or email" id="example-search-input">
+            <span class="input-group-append">
+                <button class="btn btn-sm btn-outline-secondary border-left-0 border" type="button">
+                <i class="fa fa-search"></i></button>
+            </span>
+        </form>
     </div>
 
     <!-- write article, logout, user profile -->
-	<div class="col-sm-3">
+	<div class="col-sm-3 col-md-3 col-lg-3 mx-auto">
     
         <?php if (check_session()): ?>
 
-            <a class="btn btn-small btn-outline-primary" href="<?="user_profile.php";?>">My account</a>
-            <a class="btn btn-small btn-outline-primary" href="<?="write_article.php";?>">write article</a>
-            <a class="btn btn-small btn-outline-primary" href="<?="includes/logout.php";?>">Log out</a>
+            <a class="btn btn-sm btn-outline-primary" href="<?="user_profile.php";?>">My account</a>
+            <a class="btn btn-sm btn-outline-primary" href="<?="write_article.php";?>">write article</a>
+            <a class="btn btn-sm btn-outline-primary" href="<?="includes/logout.php";?>">Log out</a>
             
         <?php endif; ?>
         
