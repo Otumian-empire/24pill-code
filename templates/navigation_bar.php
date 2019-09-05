68346443faab79f4dfe80d2a554767495c96c4aa
otumian <popecan1000@gmail.com>
2019-09-05 17:07:24 +0000

    templates/header.php - changed ./ to ./index.php

--- templates/navigation_bar.php
+++ templates/navigation_bar.php
@@ -12,14 +12,14 @@
 
     <!-- logo -->
     <div class="col-sm-1 col-md-1 col-lg-1 mx-auto">
-        <a class="navbar-brand col-sm-12 col-md-12 col-lg-12" href="./">
+        <a class="navbar-brand col-sm-12 col-md-12 col-lg-12" href="./index.php?msg=home page">
             <img src="<?="statics/img/24pill-code-blue.png";?>" alt="logo-brand" width="40" height="30">
         </a>
     </div>
 	
     <!-- nav links -->
     <div class="col-sm-2 col-md-2 col-lg-2 mx-auto">
-        <a class="btn btn-sm btn-outline-primary" href="<?="./";?>">Recent</a>
+        <a class="btn btn-sm btn-outline-primary" href="<?="./index.php?msg=recent articles";?>">Recent</a>
         <a class="btn btn-sm btn-outline-primary" href="<?="all_articles.php";?>">Articles</a>
     </div>
     
