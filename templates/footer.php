c808c7002c667008553ef1c50b798068f4906604
otumian <popecan1000@gmail.com>
2019-09-09 16:27:24 +0000

    added the CDNs but i only got the js for font-awesome

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,16 @@
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <!-- cdn static files -->
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <!-- <script src="statics/js/jquery-3.4.1.js"></script> -->
-    <!-- <script src="statics/js/bootstrap.js"></script> -->
-    <!-- <script src="statics/js/comments.js"></script> -->
-    <!-- <script src="statics/js/ckeditor.js"></script>
-    <script src="statics/js/ckeditor_configuration.js"></script> -->
-    <!-- <script src="https://cdn.ckeditor.com/ckeditor5/12.3.1/classic/ckeditor.js"></script> -->
+    <script src="https://use.fontawesome.com/b44384e9b2.js"></script> -->
+
+
+    <!-- my static files -->
+    <script src="statics/js/jquery-3.4.1.js"></script>
+    <script src="statics/js/bootstrap.js"></script>
+    <script src="statics/js/comments.js"></script>
+    
     <script src="statics/js/custom.js"></script>
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- static files -->
-    <!-- <link rel="stylesheet" href="statics/css/bootstrap.css"> -->
-    <!-- <link rel="stylesheet" href="statics/css/font-awesome-4.7.0/css/font-awesome.min.css"> -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- cdn static files -->
     
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+
+    <!-- my statics files -->
+    <link rel="stylesheet" href="statics/css/bootstrap.css">
+    <link rel="stylesheet" href="statics/css/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="statics/img/24pill-code-blue.png" type="image/x-icon">
     <link rel="stylesheet" href="statics/css/custom.css">
 
