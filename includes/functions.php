e050f02d8d85f542544944a987c5a0a6ecb7a625
otumian <popecan1000@gmail.com>
2019-09-13 04:52:05 +0000

    added a function to return all comment_ids

--- includes/functions.php
+++ includes/functions.php
@@ -469,6 +469,29 @@
 
 
     /**
+     * return an array of all the ids in the articles table
+     */
+    function select_comment_ids()
+    {
+        $db_connection = $GLOBALS['db_connection'];
+
+        $select_ids_query = "SELECT `comment_id` FROM `comments`";
+        $select_ids_result = mysqli_query($db_connection, $select_ids_query);
+        
+        if (!$select_ids_result) {
+            return 0;
+        }
+
+        if (mysqli_num_rows($select_ids_result) < 1) {
+            return 0;
+        }
+        
+        return mysqli_fetch_all($select_ids_result);
+    }
+
+
+
+    /**
      * return an assoc array of post_title, post_content, post_date and user_email
      * @param $which_has_this_id
      */
