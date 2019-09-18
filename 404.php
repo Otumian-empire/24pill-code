beddeb1b47dd5a29be87c07acfe36e4e91e8d73e
otumian <popecan1000@gmail.com>
2019-09-18 13:03:47 +0000

    added a simple 404 page

---
+++ 404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="statics/css/bootstrap.css">
+    <link rel="stylesheet" href="statics/css/custom.css">
+</head>
+<body>
+    
+
+    <div class="index-body container">
+        <div class="page-wrap d-flex flex-row align-items-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12 text-center">
+                        <span class="display-1 d-block">404</span>
+                        <div class="mb-4 lead">The page you are looking for was not found.</div>
+                        <a href="index.php" class="btn btn-link">Back to Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
