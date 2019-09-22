61577d2f3796091d67e823ec2d1f1a8577147dc1
otumian <popecan1000@gmail.com>
2019-09-22 23:01:22 +0000

    added get lines to all fields

--- signup.php
+++ signup.php
@@ -28,7 +28,7 @@
 						<div class="col-md-6">
 							<!-- first name -->
 							<div class="form-label-group">
-								<input type="text" class="form-control" id="first_name" name="sign_up_first_name" placeholder="First name" value="" autofocus/>
+								<input type="text" class="form-control" id="first_name" name="sign_up_first_name" placeholder="First name" value="<?php if (isset($_GET['first_name'])) { echo urldecode($_GET['first_name']); } ?>" autofocus/>
 								<label for="first name" class="text-capitalize">First name</label>
 							</div>
 
@@ -37,7 +37,7 @@
 						<div class="col-md-6">
 							<!-- last name -->
 							<div class="form-label-group">
-								<input type="text" class="form-control" id="last_name" name="sign_up_last_name" placeholder="Last name" value="" />
+								<input type="text" class="form-control" id="last_name" name="sign_up_last_name" placeholder="Last name" value="<?php if (isset($_GET['last_name'])) { echo urldecode($_GET['last_name']); } ?>" />
 								<label for="last name" class="text-capitalize">Last name</label>
 							</div>
 
@@ -51,7 +51,7 @@
 				<!-- email -->
 				<div class="form-group">
 					<div class="form-label-group">
-						<input type="email" class="form-control" id="email" name="sign_up_email" placeholder="Name@example.com" value="" />
+						<input type="email" class="form-control" id="email" name="sign_up_email" placeholder="Name@example.com" value="<?php if (isset($_GET['sign_up_email'])) { echo urldecode($_GET['sign_up_email']); } ?>" />
 						<label for="email">Email address</label>
 					</div>
 				</div>
@@ -89,7 +89,7 @@
 
 					<div class="form-label-group">
 
-						<textarea class="form-control" id="user_bio" name="sign_up_user_bio" rows="3" placeholder="About me" value=""></textarea>
+						<textarea class="form-control" id="user_bio" name="sign_up_user_bio" rows="3" placeholder="About me"><?php if (isset($_GET['user_bio'])) { echo urldecode($_GET['user_bio']); } ?></textarea>
 						<label for="user description" class="text-capitalize">Bio</label>
 
 					</div>
